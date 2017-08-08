26629e5bd42f776eb6975ad74304b578b9130f62
Simon Torres <storres@ctio.noao.edu>
2017-08-08 16:59:09 -0400

    added all authors

--- user_manual/conf.py
+++ user_manual/conf.py
@@ -38,7 +38,7 @@ extensions = ['sphinx.ext.todo',
 pdf_documents = [('user_manual',
                   u'user_manual',
                   u'Goodman HTS Pipeline User Manual',
-                  u'Simón Torres'), ]
+                  u'Simón Torres, Cesar Briceno and Bruno Quint'), ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
